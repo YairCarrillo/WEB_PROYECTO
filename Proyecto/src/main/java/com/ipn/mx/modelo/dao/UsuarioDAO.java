eea2d22a4b8a0a78f18da2535ea888395aa6ec17
Eduardo Yair Carrillo Balcazar <yirz.carrillo@gmail.com>
2020-06-16 16:13:40 -0500

    Udate Usuario DAO

--- Proyecto/src/main/java/com/ipn/mx/modelo/dao/UsuarioDAO.java
+++ Proyecto/src/main/java/com/ipn/mx/modelo/dao/UsuarioDAO.java
@@ -5,10 +5,88 @@
  */
 package com.ipn.mx.modelo.dao;
 
+import com.ipn.mx.modelo.dto.UsuarioDTO;
+import com.ipn.mx.util.HibernateUtil;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
 /**
  *
  * @author yirz
  */
 public class UsuarioDAO {
+    public void create(UsuarioDTO dto){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        
+        try{
+            transaction.begin();
+            session.save(dto.getEntidad());
+            transaction.commit();
+        }catch(HibernateException he){
+            if(transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+        }
+    }
     
+    public void update(UsuarioDTO dto){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        
+        try{
+            transaction.begin();
+            session.update(dto.getEntidad());
+            transaction.commit();
+        }catch(HibernateException he){
+            if(transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+        }
+    }
+    
+    public void delete(UsuarioDTO dto){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        
+        try{
+            transaction.begin();
+            session.delete(dto.getEntidad());
+            transaction.commit();
+        }catch(HibernateException he){
+            if(transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+        }
+    }
+    
+    public UsuarioDTO read(UsuarioDTO dto){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        
+        try{
+            transaction.begin();
+            dto.setEntidad(session.get(dto.getEntidad().getClass(), dto.getEntidad().getIdusuario()));
+            transaction.commit();
+        }catch(HibernateException he){
+            if(transaction != null && transaction.isActive()){
+                transaction.rollback();
+            }
+        }
+        return dto;
+    }
+    
+    public List<UsuarioDTO> readAll(){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction transaction = session.getTransaction();
+        transaction.begin();
+        List<UsuarioDTO> lista;
+        Query query = session.createQuery("from usuario h order by h.idusuario");
+        lista = query.list();
+        transaction.commit();
+        return lista;       
+    }
 }
